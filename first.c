e9293861c4044074dee6105aeab3bfc3886869fd
Tiago10Cardoso10 <tiago2003fb@gmail.com>
2023-10-12 22:15:32 +0100

    ddddd

--- first.c
+++ first.c
@@ -1,8 +1,10 @@
-int main(void) {
-    char i = 'A';
-    while ( i <= 'Z') {
-        putchar (i);
-        i = i + 1;
-    }
-    return 0;
-}
+a23 r_5673 ola_4532 adeus__r_2 _ola ___ rt
+12lp
+e___
+45_ _45
+D_4
+EEEE__
+4e _
+_e3_r
+_S34
+_Rty
