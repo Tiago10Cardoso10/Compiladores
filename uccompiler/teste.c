618e74a27e3129d249220a6fe4b079e29edb5569
kikobras <kikobras15@gmail.com>
2023-11-18 22:59:56 +0000

    caga

--- uccompiler/teste.c
+++ uccompiler/teste.c
@@ -1,9 +1,49 @@
-int main(void);
-int main(void, int);
-void chara(int integer, double, void a , double , int , char );
-int a =2;
-void as = 2+2;
-double a= 2-2-2-2*2*2,bs=((((((((((((2))))))+2)))))), test;
-void a = 2-(as-(aasd) + 2*2), b;
-int test(void a, int);
-void new(void);
+void soma(int a, char b);
+void subt(int, char, double);
+
+int a=3, b=5;
+int d=3, c=5; double t=3;
+
+int chama(void) {
+    if (o==e) {
+        ;;;
+        while (1) {
+            ;;;
+        }
+        return a;
+        ;;
+    }
+    return c;
+    ;;
+    (chama(4));
+    return b;
+}
+
+
+int main(void, void) {
+    char a= -4+3-(-3);
+    int b=a+4-(-a);
+    char a=5;
+    a=5;
+    a==5;
+    a<5;;;;;
+
+    while(!a)
+        if (2) a=3+3;
+        else a=r;
+
+    while ( ( a == 3 && b < a ) || a < 3 ) {
+        a=b;
+        b=3;
+        while (1) {
+            a=0;
+        }
+        c=5;
+    }
+
+    if ((a==3 && e==3&5))
+        return b;
+    else;
+
+    return a;
+}
