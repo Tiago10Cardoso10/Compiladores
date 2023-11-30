f1b956eb636d790b066d13c4685604ef203b5fda
kikobras <kikobras15@gmail.com>
2023-11-30 15:43:36 +0000

    safoda

---
+++ uccompiler/y.tab.h
@@ -0,0 +1,166 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    CHAR = 258,                    /* CHAR  */
+    ELSE = 259,                    /* ELSE  */
+    WHILE = 260,                   /* WHILE  */
+    IF = 261,                      /* IF  */
+    INT = 262,                     /* INT  */
+    SHORT = 263,                   /* SHORT  */
+    DOUBLE = 264,                  /* DOUBLE  */
+    RETURN = 265,                  /* RETURN  */
+    VOID = 266,                    /* VOID  */
+    BITWISEAND = 267,              /* BITWISEAND  */
+    BITWISEOR = 268,               /* BITWISEOR  */
+    BITWISEXOR = 269,              /* BITWISEXOR  */
+    AND = 270,                     /* AND  */
+    ASSIGN = 271,                  /* ASSIGN  */
+    MUL = 272,                     /* MUL  */
+    COMMA = 273,                   /* COMMA  */
+    DIV = 274,                     /* DIV  */
+    EQ = 275,                      /* EQ  */
+    GE = 276,                      /* GE  */
+    GT = 277,                      /* GT  */
+    LBRACE = 278,                  /* LBRACE  */
+    LE = 279,                      /* LE  */
+    LPAR = 280,                    /* LPAR  */
+    LT = 281,                      /* LT  */
+    MINUS = 282,                   /* MINUS  */
+    MOD = 283,                     /* MOD  */
+    NE = 284,                      /* NE  */
+    NOT = 285,                     /* NOT  */
+    OR = 286,                      /* OR  */
+    PLUS = 287,                    /* PLUS  */
+    RBRACE = 288,                  /* RBRACE  */
+    RPAR = 289,                    /* RPAR  */
+    SEMI = 290,                    /* SEMI  */
+    RESERVED = 291,                /* RESERVED  */
+    IDENTIFIER = 292,              /* IDENTIFIER  */
+    NATURAL = 293,                 /* NATURAL  */
+    DECIMAL = 294,                 /* DECIMAL  */
+    CHRLIT = 295                   /* CHRLIT  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
+#define CHAR 258
+#define ELSE 259
+#define WHILE 260
+#define IF 261
+#define INT 262
+#define SHORT 263
+#define DOUBLE 264
+#define RETURN 265
+#define VOID 266
+#define BITWISEAND 267
+#define BITWISEOR 268
+#define BITWISEXOR 269
+#define AND 270
+#define ASSIGN 271
+#define MUL 272
+#define COMMA 273
+#define DIV 274
+#define EQ 275
+#define GE 276
+#define GT 277
+#define LBRACE 278
+#define LE 279
+#define LPAR 280
+#define LT 281
+#define MINUS 282
+#define MOD 283
+#define NE 284
+#define NOT 285
+#define OR 286
+#define PLUS 287
+#define RBRACE 288
+#define RPAR 289
+#define SEMI 290
+#define RESERVED 291
+#define IDENTIFIER 292
+#define NATURAL 293
+#define DECIMAL 294
+#define CHRLIT 295
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 17 "uccompiler.y"
+
+    char *v;
+    struct node *no;
+
+#line 152 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
